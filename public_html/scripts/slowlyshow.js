20ef5069f2406087d1db687b025fee0612e431e5
SjoerdGn <38759766+SjoerdGn@users.noreply.github.com>
2022-02-02 23:09:57 +0100

    developer instead of programmer

--- public_html/scripts/slowlyshow.js
+++ public_html/scripts/slowlyshow.js
@@ -3,23 +3,20 @@ function replaceText() {
     console.log(obj);
     for (let i = 0; i < 1000; i++) {
         let k = i;
-        setTimeout(function () {
+        setTimeout(function() {
             if (k % 4 == 0) {
                 obj.innerText = 'I am a data scientist';
-            
-            }
-            else if (k % 4 == 1) {
+
+            } else if (k % 4 == 1) {
                 obj.innerText = 'I am a hydrologist';
-            
-            }
-            else if (k % 4 == 2) {
+
+            } else if (k % 4 == 2) {
                 obj.innerText = 'I am a cyclist';
-            
+
+            } else {
+                obj.innerText = 'I am a hobby developer';
             }
-            else {
-                obj.innerText = 'I am a hobby programmer';
-            }
-        }, 1500*(k+2));
+        }, 1500 * (k + 2));
 
     }
 }
