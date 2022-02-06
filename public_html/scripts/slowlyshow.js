0d08ef64bd9a8ba4321887b62936173a0fd88939
SjoerdGn <38759766+SjoerdGn@users.noreply.github.com>
2022-02-06 22:06:40 +0100

    typewriter and icons in chrome and edge

--- public_html/scripts/slowlyshow.js
+++ public_html/scripts/slowlyshow.js
@@ -1,29 +1,66 @@
 $(window).scroll(function() {
-    $(".top").css("opacity", 1 - $(window).scrollTop() / 600);
+    $(".openPage").css("opacity", 1 - $(window).scrollTop() / 600);
 });
 
-function replaceText() {
-    obj = document.getElementById("slowlyShow");
-    console.log(obj);
-    for (let i = 0; i < 1000; i++) {
-        let k = i;
-        setTimeout(function() {
-            if (k % 4 == 0) {
-                obj.innerText = 'I am a data scientist';
 
-            } else if (k % 4 == 1) {
-                obj.innerText = 'I am a hydrologist';
 
-            } else if (k % 4 == 2) {
-                obj.innerText = 'I am a cyclist';
 
-            } else {
-                obj.innerText = 'I am a hobby developer';
-            }
-        }, 1500 * (k + 2));
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
 
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-}
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewriter');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewriter > .wrap { border-right: 0.08em solid #d4f2db}";
+    document.body.appendChild(css);
+};
 
 
 
